4879ab5c78ec77076cccdeafc0a5595094845a71
cercxtrova <robinhernie11@gmail.com>
2022-07-08 20:26:40 +0200

    LineItemDate added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
-  
+
   devise_for :users
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: %i[index show]
+  end
 end
